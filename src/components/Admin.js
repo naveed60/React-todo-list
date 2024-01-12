45f542d8081d830951474fa0b6c2a17704872a0a
naveed60 <advmiannaveedali@gmail.com>
2024-01-12 13:05:37 +0500

    admin component updated

---
+++ src/components/Admin.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Admin = () => {
+  return (
+    <div>
+      <h2>Admin Dashboard</h2>
+      {/* Admin-specific content goes here */}
+    </div>
+  );
+};
+
+export default Admin;
